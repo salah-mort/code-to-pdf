ab9fe0253433826c462b994998a6e9ef9df38d5f
Salah Mutaja <82736192+salah-mort@users.noreply.github.com>
2025-03-17 12:52:06 +0200

    Delete gmailinboxreading directory

--- gmailinboxreading/src/main/java/com/example/constants/ProjectConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.constants;
-
-public class ProjectConstants {
-
-	public static final String HOST = "imap.gmail.com";
-	public static final String PROTOCOL = "imaps";
-	public static final String MAIL_STORE_PROTOCOL = "mail.store.protocol";
-}
